2e862d7802a5dac8ae05c9f16f210976c1bf2c51
EisnebahenrFlo <f.kohn1987@gmail.com>
2026-05-15 22:29:49 +0200

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,4 @@
 export const APP_NAME = "MeteoFlo";
 export const APP_TAGLINE = "Wetter mit KI-Analyse für DACH & Italien";
+export const APP_VERSION = "1.0.0-rc1";
+export const APP_BUILD_DATE = "2026-05-15";
